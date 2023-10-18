33025f1cf369f8cc2aaac61906143349068afeb0
Blaze Fugina <blaze@Blazes-MacBook-Pro.local>
2023-10-17 23:18:42 -0500

    Better backend structure.

--- routes/server.go
+++ routes/server.go
@@ -1,32 +1,10 @@
-package server
+package routes
 
 import (
-	"context"
-	"os"
-
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Race struct {
-	ID           primitive.ObjectID `bson:"_id"`
-	RaceName     string             `bson:"raceName"`
-	RaceDate     string             `bson:"raceDate"`
-	RaceDistance float64            `bson:"raceDistance"`
-	PercentBack  float64            `bson:"percentBack"`
-}
 
-type CreateRace struct {
-	RaceName     string             `json:"raceName" bson:"raceName"`
-	RaceDate     string             `json:"raceDate" bson:"raceDate"`
-	RaceDistance float64            `json:"raceDistance" bson:"raceDistance"`
-	PercentBack  float64            `json:"percentBack" bson:"percentBack"`
-	UserID       primitive.ObjectID `json:"userId" bson:"userId"`
-}
 
 func NewRouter() {
 	e := gin.Default()
@@ -35,115 +13,7 @@ func NewRouter() {
 		c.String(200, "Nordic percent back.")
 	})
 
-	e.GET("/races/:objectId", func(c *gin.Context) {
-		id := c.Param("objectId")
-
-		serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-		err := godotenv.Load()
-		if err != nil {
-			panic(err)
-		}
-		connectionURI := os.Getenv("mongo_uri")
-
-		opts := options.Client().ApplyURI(connectionURI).SetServerAPIOptions(serverAPI)
-		// Create a new client and connect to the server
-		client, err := mongo.Connect(context.TODO(), opts)
-		if err != nil {
-			panic(err)
-		}
-
-		objectId, err := primitive.ObjectIDFromHex(id)
-		if err != nil {
-			panic(err)
-		}
-
-		coll, err := client.Database("percent-back-app").Collection("races").Find(context.TODO(), bson.M{"userId": objectId})
-		if err != nil {
-			panic(err)
-		}
-		defer coll.Close(context.TODO())
-
-		races := make([]Race, 0)
-		for coll.Next(context.TODO()) {
-			var race Race
-			if err := coll.Decode(&race); err != nil {
-				panic(err)
-			}
-			races = append(races, race)
-		}
-		c.JSON(200, races)
-	})
-
-	e.POST("/races/create", func(c *gin.Context) {
-		var race CreateRace
-
-		err := c.Bind(&race)
-		if err != nil {
-			panic(err)
-		}
-
-		serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-		err = godotenv.Load()
-		if err != nil {
-			panic(err)
-		}
-		connectionURI := os.Getenv("mongo_uri")
-
-		opts := options.Client().ApplyURI(connectionURI).SetServerAPIOptions(serverAPI)
-		// Create a new client and connect to the server
-		client, err := mongo.Connect(context.TODO(), opts)
-		if err != nil {
-			panic(err)
-		}
-
-		coll := client.Database("percent-back-app").Collection("races")
-		if err != nil {
-			panic(err)
-		}
-
-		doc := CreateRace{RaceName: race.RaceName, RaceDate: race.RaceDate, RaceDistance: race.RaceDistance, PercentBack: race.PercentBack, UserID: race.UserID}
-		result, err := coll.InsertOne(context.TODO(), doc)
-		if err != nil {
-			panic(err)
-		}
-
-		c.JSON(200, result)
-	})
-
-	e.DELETE("/races/delete/:id", func(c *gin.Context) {
-		id := c.Param("id")
-
-		serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-		err := godotenv.Load()
-		if err != nil {
-			panic(err)
-		}
-		connectionURI := os.Getenv("mongo_uri")
-
-		opts := options.Client().ApplyURI(connectionURI).SetServerAPIOptions(serverAPI)
-		// Create a new client and connect to the server
-		client, err := mongo.Connect(context.TODO(), opts)
-		if err != nil {
-			panic(err)
-		}
-
-		objectId, err := primitive.ObjectIDFromHex(id)
-		if err != nil {
-			panic(err)
-		}
-
-		coll := client.Database("percent-back-app").Collection("races")
-		if err != nil {
-			panic(err)
-		}
-
-		result, err := coll.DeleteOne(context.TODO(), bson.M{"_id": objectId})
-		if err != nil {
-			panic(err)
-		}
-
-		c.JSON(200, result)
-	})
+	racesRoutes(e)
 
 	err := e.Run()
 	if err != nil {
